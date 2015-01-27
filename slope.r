cc251751cdf81a9401299e7caca58e369337aa67
Filip Kral <filipking@gmail.com>
2015-01-27 14:39:28 +0000

    Add slope.r as a simple demo

---
+++ slope.r
@@ -0,0 +1,30 @@
+# Calculate slope and aspect of a digital terrain model
+
+slope<-function(r, res=1){
+  # returns a slope surface of elevation raster r of resolution res,
+  # according to Burrough et McDonnell 2000, p. 191
+  newr<-matrix(0, nrow=nrow(r), ncol=ncol(r))
+  h<-nrow(r)
+  w<-ncol(r)
+  for(i in 2:(h-1)){
+    for(j in 2:(w-1)){
+      newr[i,j]<-sqrt( ((r[i+1,j]-r[i-1,j])/(2*res))^2 + ((r[i,j+1]-r[i,j-1])/(2*res))^2 )
+    }
+  }
+  newr[1,]<-0; newr[h,]<-0; newr[,1]<-0; newr[,w]<-0; #adjust border
+  return(newr)
+}
+
+
+# parameters
+input_terrain <- 'c:/temp/terrain.txt'
+
+# read data
+m<-as.matrix(read.table(input_terrain))
+
+# calculate slope
+s<-slope(m)
+
+# plot results
+image(m, main="elevation")
+image(s, main="slope")
